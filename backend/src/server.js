b8ecb359c08bb967fe75640ad27efdc6795fb80e
Affan-Codes <affan.khan4cs@gmail.com>
2025-07-27 12:40:49 +0530

    Backend mostly completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import usersRoutes from "./routes/users.route.js";
+import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 connectDB();
@@ -11,9 +14,12 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
